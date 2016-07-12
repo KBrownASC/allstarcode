c755d72685a1d9cabd714d9aa39ce1de41e2901f
KBrownASC <Keroneobrownjr@gmail.com>
2016-07-12 11:39:05 -0400

    SquareMyro
    The code for the sim of the Myro bot so it can go in a square

---
+++ SquareMyro.py
@@ -0,0 +1,15 @@
+from Myro import *
+init("sim")
+
+size=2
+time=1
+bigTime=3
+turnRight = 90
+
+forward(size,time)
+turnBy(turnRight,"deg")
+forward(size,time)
+turnBy(turnRight, "deg")
+forward(size,time)
+turnBy(turnRight,"deg")
+forward(size,time)
