deb43ae93c6369d44ee5e6de0b6ee725aeb4fd6f
KBrownASC <Keroneobrownjr@gmail.com>
2016-08-02 09:59:35 -0400

    webbby

--- Website/ball.js
+++ Website/ball.js
@@ -5,11 +5,12 @@ var canvas;
 
 function setup() {
 	canvas = createCanvas(800,800);
-	hoop = loadImage("hoop.png");
+	
 }
 
 function draw() {
 	background(220,180,90);
+	hoop = loadImage("hoop.png");
 	image(hoop,0,0);
 
 }
